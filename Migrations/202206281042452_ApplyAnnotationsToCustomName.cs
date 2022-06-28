c986d3b3f9b64fec86d558d0443281f532cd1d0b
LukaDrozdek <Luka04111993@gmail.com>
2022-06-28 12:44:07 +0200

    migracija
    dodavanja lengta na ime 255

---
+++ Migrations/202206281042452_ApplyAnnotationsToCustomName.cs
@@ -0,0 +1,18 @@
+namespace Filmovi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplyAnnotationsToCustomName : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+        }
+    }
+}
