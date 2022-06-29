7afe91382587efd47ad636af12686e3b333eea91
LukaDrozdek <Luka04111993@gmail.com>
2022-06-29 02:49:20 +0200

    Zadatak 1

--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -8,6 +8,7 @@ namespace Filmovi.Models
     public class MembershipType
     {
         public byte Id { get; set; }
+        public string Name { get; set; }
         public short SingUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
