d2613a6522185074c3f80b308f219938789b1816
LukaDrozdek <Luka04111993@gmail.com>
2022-06-30 02:51:20 +0200

    Lekcija 2 zavrsena

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -8,18 +8,32 @@ using System.Web.Mvc;
 
 namespace Filmovi.Controllers
 {
-    public class MovieController : Controller
+    public class MoviesController : Controller
     {
-        // GET: Movie
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
-
-            var movie = new Movie() { Name = "Lord of the rings" };
-
+            var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer {Name = "Kupac01"},
-                new Customer {Name = "Kupac02"}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
@@ -27,6 +41,7 @@ namespace Filmovi.Controllers
                 Movie = movie,
                 Customers = customers
             };
+
             return View(viewModel);
         }
     }
