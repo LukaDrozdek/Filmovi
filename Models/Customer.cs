beafab03714a80f2b08bda7565c6b80ec5f34059
LukaDrozdek <Luka04111993@gmail.com>
2022-06-30 03:30:31 +0200

    Migracija lekcija 26

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -10,7 +10,7 @@ namespace Filmovi.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
-        //public MembershipType MembershipType { get; set; }
-        //public byte MembershipTypeId { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -7,5 +7,9 @@ namespace Filmovi.Models
 {
     public class MembershipType
     {
+        public byte Id { get; set; }
+        public short SingUpFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
     }
 }
